641e506fec2fcc70954d56653590d1d3a56fcad1
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-02 10:34:59 -0600

    Refactor reducer with ternary

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,14 +1,16 @@
 export const todos = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, { id: Date.now(), todo: action.todo, completed: false }];
+      return [...state, 
+        { 
+        id: Date.now(), 
+        todo: action.todo, 
+        completed: false 
+        }];
     case 'UPDATE_STATUS':
-      return state.map(todo => {
-        if (action.id === todo.id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      });
+      return state.map(todo => 
+        action.id === todo.id ? { ...todo, completed: !todo.completed } : todo
+      );
     default:
       return state;
   }
